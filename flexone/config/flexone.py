e9303c7bf589dd8b4b8ff3139bb89af3a5614273
ashish-greycube <mr.ashish.shah@gmail.com>
2018-04-09 11:35:47 +0530

    removed flexone module from side bar

--- flexone/config/flexone.py
+++ flexone/config/flexone.py
@@ -10,51 +10,5 @@ def get_data():
 					"name": "dashboard",
 				}
 			]
-		},
-				{
-			"label": _("Accounts"),
-			"items": [
-
-				{
-					"type": "doctype",
-					"name": "Journal Entry",
-					"description": _("Bills raised by Suppliers.")
-				},
-				{
-					"type": "doctype",
-					"name": "Payment Request",
-					"description": _("Payment Request")
-				},
-				{
-					"type": "doctype",
-					"name": "Payment Entry",
-					"description": _("Bank/Cash transactions against party or for internal transfer")
-				},
-				{
-					"type": "page",
-					"name": "pos",
-					"label": _("POS"),
-					"description": _("Point of Sale")
-				},
-				{
-					"type": "doctype",
-					"name": "Subscription",
-					"label": _("Subscription"),
-					"description": _("To make recurring documents")
-				},
-				{
-					"type": "report",
-					"name": "Accounts Receivable",
-					"doctype": "Sales Invoice",
-					"is_query_report": True
-				},
-				{
-					"type": "report",
-					"name": "Accounts Payable",
-					"doctype": "Purchase Invoice",
-					"is_query_report": True
-				},
-			]
-
-		},
+		}
     ]
