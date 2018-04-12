c345d48ee40f07dd5a332f5619b839e0c9609ec6
ashish-greycube <mr.ashish.shah@gmail.com>
2018-04-12 19:28:02 +0530

    when no data in due amount solved

--- flexone/flexone/page/dashboard/dashboard.py
+++ flexone/flexone/page/dashboard/dashboard.py
@@ -60,15 +60,22 @@ def due_amount():
 	company = erpnext.get_default_company()
 	start_date = frappe.db.sql("""select min(posting_date) from `tabSales Invoice` where company = %s""", (company))[0][0] or today()
 	custom_filter = {'from_date': start_date ,'to_date': today(),'company': company}
-	check_data=frappe.db.sql("""SELECT count(name) FROM `tabSales Invoice` WHERE `tabSales Invoice`.docstatus = 1 and company = %s and posting_date >= %s and posting_date <= %s  """, (company,start_date,today()))[0][0] 
-	if cint(check_data)==0:
-		return 'Due Amount',0
-	else:
+
+	due_amount=0
+
+	if frappe.db.exists("""
+		SELECT 1 FROM `tabSales Invoice` 
+		WHERE `tabSales Invoice`.docstatus = 1 and 
+		company = %s and posting_date >= %s and posting_date <= %s
+		""", (company,start_date,today())):
 		report = frappe.get_doc('Report', "Sales Register") 
 		columns, data = report.get_data(filters = custom_filter, as_dict=True)
-		sales_abbr="Sales - {}".format(frappe.db.get_value('Company', company, 'abbr'))	
+		# sales_abbr="Sales - {}".format(frappe.db.get_value('Company', company, 'abbr'))	
 		list_of_total_outstanding_amount = [i[_("Outstanding Amount")] for i in data if _("Outstanding Amount") in i]
-		return 'Due Amount',list_of_total_outstanding_amount[-1]
+		due_amount = list_of_total_outstanding_amount[-1]
+	return 'Due Amount', due_amount
+
+
 
 @frappe.whitelist()
 def top_moving_items():
